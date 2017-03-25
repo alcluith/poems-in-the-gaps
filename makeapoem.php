e762e804aa85081712ba8ba696e80ebfb8e26a8c
Claire Quigley <claire.quigley@gmail.com>
2017-03-25 19:38:27 +0000

    add stats

--- makeapoem.php
+++ makeapoem.php
@@ -163,7 +163,24 @@ Select text to work with:
 
   <div id="textpage">
   </div>
-
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=11295729; 
+var sc_invisible=1; 
+var sc_security="5ad8cd79"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="free web stats"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="//c.statcounter.com/11295729/0/5ad8cd79/1/" alt="free
+web stats"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
   
  </body>
+ 
 </html>
